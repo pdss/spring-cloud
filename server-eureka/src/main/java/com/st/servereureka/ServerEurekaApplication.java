56ee30b8c9db2c0f1293c35ff163a39a98e4a838
pdss <307958201@qq.com>
2019-03-04 11:24:30 +0800

    as

--- server-eureka/src/main/java/com/st/servereureka/ServerEurekaApplication.java
+++ server-eureka/src/main/java/com/st/servereureka/ServerEurekaApplication.java
@@ -3,7 +3,7 @@ package com.st.servereureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//eureka服务器
 @EnableEurekaServer
 @SpringBootApplication
 public class ServerEurekaApplication {
